9ba82420d2c5c230f9b4b0a086664aeba08df85b
MateuszGacek <mateusz_gacek@interia.pl>
2024-05-20 20:54:02 +0200

    feat: create addItem with reducer in context

--- src/components/store/CartContext.tsx
+++ src/components/store/CartContext.tsx
@@ -13,11 +13,41 @@ const CartContext = createContext<CartContextValue>({
   removeItem: (id) => {},
 });
 
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.item[existingCartItemIndex];
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    //....
+  }
+
+  return state;
+}
+
 export function CartContextProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useReducer(cartReducer, { items: [] });
   return <CartContext.Provider>{children}</CartContext.Provider>;
 }
 
